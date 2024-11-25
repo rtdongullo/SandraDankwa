127aff2d4fec1592638715ede3fc59ef687eaacc
rtdongullo <dongromert@gmail.com>
2024-11-25 11:31:52 +0000

    pushing initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=" h-screen flex flex-col justify-center items-center">
+        <p className="font-bold text-6xl">
+          Sandra Dankwa
+        </p>
+        <p className="text-blue-800 font-semibold animate-pulse text-3xl">
+          Innovator of the year
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
